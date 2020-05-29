8e0542c0267669bb775c93db13f099def527c490
Tunarong <cnskgus201@naver.com>
2020-05-29 18:20:16 +0900

    pds 기능 추가~

---
+++ src/main/java/tunarong/spring/mvc/service/MallsService.java
@@ -0,0 +1,40 @@
+package tunarong.spring.mvc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tunarong.spring.mvc.dao.BoardDAO;
+import tunarong.spring.mvc.vo.BoardVO;
+
+import java.util.ArrayList;
+
+@Service("bsrv")
+public class MallsService {
+
+    private BoardDAO bdao;
+
+    @Autowired
+    public MallsService(BoardDAO bdao) {
+        this.bdao = bdao;
+    }
+
+    public String newBoard(BoardVO bd) {
+        String result = "데이터 입력 실패!";
+
+        if (bdao.insertBoard(bd))
+            result = "데이터 입력 성공!!";
+
+        System.out.println(result);
+        // result 변수 값을 WAS 콘솔에 로그형태로 출력
+
+        return result;
+    }
+
+    public ArrayList<BoardVO> showBoard() {
+        return (ArrayList<BoardVO>)bdao.selectBoard();
+    }
+
+    public BoardVO showOneBoard(String bno) {
+        return bdao.selectOneBoard(bno);
+    }
+
+}
